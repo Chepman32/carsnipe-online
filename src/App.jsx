6b7379bf92e355e76bf5d4c1befd977e152558dc
Chepman32 <antonkerch555@gmail.com>
2025-04-17 19:48:06 +0300

    video.mp4

--- src/App.jsx
+++ src/App.jsx
@@ -304,7 +304,7 @@ const AppContentWrapper = ({ playerInfo, money, setMoney, currentAuthenticatedUs
             zIndex: -1
           }}
         >
-          <source src="/videos/Intro.MP4" type="video/mp4" />
+          <source src="/videos/video.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
         <div style={{
